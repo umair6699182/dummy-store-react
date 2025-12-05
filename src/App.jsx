ce46d2a2c35e957a7f9562eefcd4009cc6d14409
umair6699182 <um83414@gmail.com>
2025-12-05 18:27:57 +0500

    Some New File Add

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import './App.css'
 import ProductCard from './ProductCard'
+import SampleCard from './SampleCard'
 
 function App() {
 
   return (
     <main>
       <ProductCard />
+      <SampleCard />
     </main>
   )
 }
---
+++ src/SampleCard.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function SampleCard() {
+  return (
+    <div>
+      This is an Basically Sample Card Component
+    </div>
+  )
+}
+
+export default SampleCard
