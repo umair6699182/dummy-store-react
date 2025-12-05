5beeb6759baf14e070295bf40cf113b0ff12de41
umair6699182 <um83414@gmail.com>
2025-12-05 23:14:55 +0500

    Payment Page Added it

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,20 @@
-import './App.css'
-import ProductCard from './ProductCard'
-import SampleCard from './SampleCard'
+import "./App.css";
+import Dashboard from "./Dashboard";
+import Login from "./Login";
+import ProductCard from "./ProductCard";
+import SampleCard from "./SampleCard";
+import Payment from "./Payment";
 
 function App() {
-
   return (
     <main>
       <ProductCard />
       <SampleCard />
+      <Dashboard />
+      <Login />
+      <Payment />
     </main>
-  )
+  );
 }
 
-export default App
+export default App;
